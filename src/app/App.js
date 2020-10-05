1ab80e5b7efa79d4dd7a60a285074d9e771b75d4
CH <1925480119@qq.com>
2020-10-06 00:07:20 +0800

    [chen] addApprouter

--- src/app/App.js
+++ src/app/App.js
@@ -1,14 +1,27 @@
 import React, {Component} from 'react';
 import './app.less';
-import {Route, BrowserRouter} from "react-router-dom";
+import {Route, BrowserRouter, Switch, Link} from "react-router-dom";
 import Home from "./Home";
+import Calculator from "./components/Calculator";
+import Timer from "./components/Timer"
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-        <BrowserRouter>
-          <Route exact path='/' component={Home}/>
+        <BrowserRouter> 
+          <ul className='menu'>
+            <li><Link to='/'>HOME</Link></li>
+            <li><Link to='/calculator'>在线计算器</Link></li>
+            <li><Link to='/timer'>在线倒计时器</Link></li>  
+          </ul>  
+          <div className='content'>        
+            <Switch>
+              <Route exact path='/' component={Home}/>
+              <Route  path='/calculator' component={Calculator}/>
+              <Route  path='/timer' component={Timer}/>
+            </Switch>
+          </div>
         </BrowserRouter>
       </div>
     );
--- src/app/Home.js
+++ src/app/Home.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import './home.less';
+import { Link} from "react-router-dom";
 
 const Home = () => {
   return (<div className="home">
-    This is home page
+     <div className="logo">在线实用工具</div>
+     <div className="content">
+       <div>
+         <Link  to='/calculator'>计算器</Link>
+        </div>
+       <div><Link  to='/timer'>倒计时器</Link></div>
+     </div>
   </div>);
 };
 
